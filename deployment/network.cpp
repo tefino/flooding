5c3678f1713fad8f7b86f0cf4d7eb9384af77987
tefino <sjsssd@126.com>
2012-05-20 16:45:55 +0800

    Update deployment/network.cpp

--- deployment/network.cpp
+++ deployment/network.cpp
@@ -370,11 +370,11 @@ void Domain::writeClickFiles(bool montoolstub) {
         click_conf<<"classifier[2]->[1]cacheunit;"<<endl ;
         click_conf<<"classifier[3]->[2]cacheunit;"<<endl ;
 
-        click_conf<<"fw[3]->[4]proxy"<<endl ;
-        click_conf<<"proxy[6]->[6]fw"<<endl ;
-        click_conf<<"cacheunit[4]->[7]fw"<<endl ;
-        click_conf<<"cacheunit[5]->[8]fw"<<endl ;
-        click_conf<<"classifier[4]->[3]cacheunit"<<endl ;
+        click_conf<<"fw[3]->[4]proxy;"<<endl ;
+        click_conf<<"proxy[6]->[6]fw;"<<endl ;
+        click_conf<<"cacheunit[4]->[7]fw;"<<endl ;
+        click_conf<<"cacheunit[5]->[8]fw;"<<endl ;
+        click_conf<<"classifier[4]->[3]cacheunit;"<<endl ;
         click_conf.close();
     }
 }
